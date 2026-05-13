8d74dae739b71dd3eae6033542a16d06eeb75a12
atishdixit16 <atishdixit16@gmail.com>
2026-05-13 11:48:10 +0530

    refactor: update input label for batch size and remove outdated example case studies and strategy documentation

--- Features/Materials_Exploration/DatabaseExtractor/DatabaseExtractor.js
+++ Features/Materials_Exploration/DatabaseExtractor/DatabaseExtractor.js
@@ -11,7 +11,7 @@ class DatabaseExtractorFeature extends BaseFeature {
         return `
             <p>Configure input parameters for Database Extractor</p>
             <div class="input-controls">
-                ${this.createNumberInput(`batchSize_${this.featureId}`, 'Batch Size', '1', '10000', '1')}
+                ${this.createNumberInput(`batchSize_${this.featureId}`, 'Max Batch Size / Database', '1', '10000', '1')}
                 ${this.createSelectInput(`retrievalMode_${this.featureId}`, 'Retrieval Mode', [{value: 'lenient', text: 'Lenient'}, {value: 'strict', text: 'Strict'}])}
                 ${this.createTextInput(`targetCompositions_${this.featureId}`, 'Target Compositions (optional)', 'e.g., Fe, Al2O3')}
                 <div>
